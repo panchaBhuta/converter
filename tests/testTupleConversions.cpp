6f13867b97d8a8037ea13bf6411e5078ae0672e3
Gautam Dhar <27485331+panchaBhuta@users.noreply.github.com>
2023-09-15 22:50:16 +0530

    added OS-version, and compiler-version

--- tests/testTupleConversions.cpp
+++ tests/testTupleConversions.cpp
@@ -84,7 +84,7 @@ int main()
 
     std::vector<std::string> expectedOutputVector{"2023-02-21", "1", "2.29999995", "-3.39999999999999991", "-5"};
     conversionVectorEqualCheck<std::chrono::year_month_day,unsigned,float,double,int>
-        ("2023-02-21,1,2.3,-3.4,-5", chkTuple, expectedOutputVector);
+        (inputVector, chkTuple, expectedOutputVector);
 #else
     conversionStringEqualCheck<std::chrono::year_month_day,unsigned,float,double,int>
         ("2023-02-21,1,2.3,-3.4,-5", chkTuple);
