d60d0e0d7b8ebdc0fdf4f9f09564e46ef180ce55
Gautam Dhar <27485331+panchaBhuta@users.noreply.github.com>
2023-10-05 14:00:25 +0530

    5:added flags for 'Windows:ClangCL' build

--- cmake/converter.cmake
+++ cmake/converter.cmake
@@ -386,7 +386,7 @@ endmacro()
 
 # Helper function to enable warnings
 macro(converter_enable_warnings)
-    set(gcc_warnings "-Wall;-Wextra;-Wpedantic;-Wshadow;-Wpointer-arith")
+    set(gcc_warnings "-Wextra;-Wpedantic;-Wshadow;-Wpointer-arith")
     set(gcc_warnings "${gcc_warnings};-Wcast-qual;-Wno-missing-braces;-Wswitch-default;-Wcast-align;-Winit-self")
     set(gcc_warnings "${gcc_warnings};-Wunreachable-code;-Wundef;-Wuninitialized;-Wold-style-cast;-Wwrite-strings")
     set(gcc_warnings "${gcc_warnings};-Wsign-conversion;-Weffc++")
@@ -403,10 +403,11 @@ macro(converter_enable_warnings)
     #]==================================================================================]
     target_compile_options(converter INTERFACE
         "$<${gcc_like_cxx}:$<BUILD_INTERFACE:${gcc_warnings}>>"
+        "$<$<AND:${gcc_like_cxx},$<NOT:${windows_os_clang_cxx}>>:$<BUILD_INTERFACE:-Wall>>"
         "$<${gcc_cxx_v5_or_later}:$<BUILD_INTERFACE:-Wsuggest-override>>"
-        "$<$<NOT:${windows_os}>:$<BUILD_INTERFACE:-g>>"
+        "$<$<NOT:${windows_os}>:$<BUILD_INTERFACE:-g>>"  # for linux and macOS
         "$<${windows_os}:$<BUILD_INTERFACE:-Z7>>"  # -Z7 is equivalent for -g
-        "$<${windows_os_clang_cxx}:$<BUILD_INTERFACE:-Wno-c++98-compat;-Wno-c++98-compat-pedantic;-Wno-extra-semi-stmt;-Wno-global-constructors;-Wno-exit-time-destructors>>"
+        #"$<${windows_os_clang_cxx}:$<BUILD_INTERFACE:-Wno-c++98-compat;-Wno-c++98-compat-pedantic;-Wno-extra-semi-stmt;-Wno-global-constructors;-Wno-exit-time-destructors>>"
         "$<${msvc_cxx}:$<BUILD_INTERFACE:-W4>>")
     #add_compile_options("/utf-8")  for msvc  -> check in cxxopts.cmake
 endmacro()
