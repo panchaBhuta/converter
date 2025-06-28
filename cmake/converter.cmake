b6e8db46769db2f28f0a30dfae0abb77951e8f92
Gautam Dhar <goti.bhai@gmail.com>
2025-06-29 02:14:29 +0530

    update for github windows workflow 8

--- cmake/converter.cmake
+++ cmake/converter.cmake
@@ -458,7 +458,8 @@ macro(converter_enable_warnings)
         "$<$<AND:${gcc_like_cxx},$<NOT:${windows_os_clang_cxx}>>:$<BUILD_INTERFACE:-Wall>>" # -Wall for 'windows_os_clang_cxx' gives lot of warnings
         "$<${gcc_cxx_v5_or_later}:$<BUILD_INTERFACE:-Wsuggest-override>>"
         "$<$<NOT:${windows_os}>:$<BUILD_INTERFACE:-g>>"  # for linux and macOS
-        "$<${windows_os}:$<BUILD_INTERFACE:-Z7>>"  # -Z7 is equivalent for -g
+        "$<$<AND:${windows_os},${gcc_cxx}>:$<BUILD_INTERFACE:-g>>"  # for g++ on windows
+        "$<$<AND:${windows_os},$<NOT:${gcc_cxx}>>:$<BUILD_INTERFACE:-Z7>>"  # -Z7 is equivalent for -g
         #"$<${windows_os_clang_cxx}:$<BUILD_INTERFACE:-Wno-c++98-compat;-Wno-c++98-compat-pedantic>>"
         #"$<${windows_os_clang_cxx}:$<BUILD_INTERFACE:-Wno-global-constructors;-Wno-exit-time-destructors>>"
         #"$<${windows_os_clang_cxx}:$<BUILD_INTERFACE:-Wno-extra-semi-stmt;-Wno-string-plus-int>>"
