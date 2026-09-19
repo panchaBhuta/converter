669a50355b439c7885f10e8777554354d601db9e
Gautam <goti.bhai@gmail.com>
2026-09-19 22:31:22 +0530

    test: isolate std::to_string in try_run

--- cmake/diagnostic_try_run/diagnostic_try_run.cpp
+++ cmake/diagnostic_try_run/diagnostic_try_run.cpp
@@ -1,4 +1,21 @@
+#include <iostream>
+#include <string>
+
 int main()
 {
+    float value = 11.0f;
+    std::string strVal = std::to_string(value);
+    std::cout << "value=11.0f result=[" << strVal << "]\n";
+
+    if(strVal != "11")
+        return -1;
+
+    value = 1.2345f;
+    strVal = std::to_string(value);
+    std::cout << "value=1.2345f result=[" << strVal << "]\n";
+
+    if(strVal != "1.2345")
+        return -1;
+
     return 0;
 }
