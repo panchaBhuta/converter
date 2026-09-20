2cd37c8bba1aa42df543a6afea4d24d8a90de160
Gautam <goti.bhai@gmail.com>
2026-09-20 12:17:14 +0530

    Diagnose MSVC assert output in try_run

--- cmake/diagnostic_try_run/diagnostic_try_run.cpp
+++ cmake/diagnostic_try_run/diagnostic_try_run.cpp
@@ -2,8 +2,17 @@
 #include <iostream>
 #include <string>
 
+#ifdef _MSC_VER
+#include <crtdbg.h>
+#endif
+
 int main()
 {
+#ifdef _MSC_VER
+    _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
+    _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+#endif
+
     float value = 11.0f;
     std::string strVal = std::to_string(value);
     std::cout << "value=11.0f result=[" << strVal << "]\n";
